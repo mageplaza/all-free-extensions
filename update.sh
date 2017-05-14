4c79b543d1ca76b2e3e6886056a641ba904e727e
Sam <sam@mageplaza.com>
2017-05-14 16:00:44 +0700

    Update

--- update.sh
+++ update.sh
@@ -2,23 +2,23 @@
 
 git pull
 
-git clone https://github.com/mageplaza/magento-2-blog
 rm -rf magento-2-blog/.git
+git clone https://github.com/mageplaza/magento-2-blog
 
-git clone https://github.com/mageplaza/magento-2-social-login
 rm -rf magento-2-social-login/.git
+git clone https://github.com/mageplaza/magento-2-social-login
 
-git clone https://github.com/mageplaza/magento-2-seo
 rm -rf magento-2-seo/.git
+git clone https://github.com/mageplaza/magento-2-seo
 
-git clone https://github.com/mageplaza/magento-2-product-slider
 rm -rf magento-2-product-slider/.git
+git clone https://github.com/mageplaza/magento-2-product-slider
 
-git clone https://github.com/mageplaza/magento-2-google-analytics
 rm -rf magento-2-google-analytics/.git
+git clone https://github.com/mageplaza/magento-2-google-analytics
 
-git clone https://github.com/mageplaza/magento-2-banner-slider
 rm -rf magento-2-banner-slider/.git
+git clone https://github.com/mageplaza/magento-2-banner-slider
 
 
 
