83e264d7cb6311b0b8e17c4facb125e57685f8cc
Sam <sam@mageplaza.com>
2018-10-16 13:34:14 +0700

    clear

--- update.sh
+++ update.sh
@@ -5,45 +5,59 @@ git pull
 
 rm -rf magento-2-security
 git clone https://github.com/mageplaza/magento-2-security
+rm -rf magento-2-security/.git
 
 rm -rf magento-2-reports
 git clone https://github.com/mageplaza/magento-2-reports
+rm -rf magento-2-reports/.git
 
 rm -rf magento-2-blog
 git clone https://github.com/mageplaza/magento-2-blog
+rm -rf magento-2-blog/.git
 
 rm -rf magento-2-multi-flat-rates
 git clone https://github.com/mageplaza/magento-2-multi-flat-rates
+rm -rf magento-2-multi-flat-rates/.git
 
 rm -rf magento-2-ajax-layered-navigation
 git clone https://github.com/mageplaza/magento-2-ajax-layered-navigation
+rm -rf magento-2-ajax-layered-navigation/.git
 
 rm -rf magento-2-social-login
 git clone https://github.com/mageplaza/magento-2-social-login
+rm -rf magento-2-social-login/.git
 
 rm -rf magento-2-google-recaptcha
 git clone https://github.com/mageplaza/magento-2-google-recaptcha
+rm -rf magento-2-google-recaptcha/.git
 
 rm -rf magento-2-better-popup
 git clone https://github.com/mageplaza/magento-2-better-popup
+rm -rf magento-2-better-popup/.git
 
 rm -rf magento-2-smtp
 git clone https://github.com/mageplaza/magento-2-smtp
+rm -rf magento-2-smtp/.git
 
 rm -rf magento-2-google-xml-sitemap
 git clone https://github.com/mageplaza/magento-2-google-xml-sitemap
+rm -rf magento-2-google-xml-sitemap/.git
 
 rm -rf magento-2-delete-orders
 git clone https://github.com/mageplaza/magento-2-delete-orders
+rm -rf magento-2-delete-orders/.git
 
 rm -rf magento-2-banner-slider
 git clone https://github.com/mageplaza/magento-2-banner-slider
+rm -rf magento-2-banner-slider/.git
 
 rm -rf magento-2-product-slider
 git clone https://github.com/mageplaza/magento-2-product-slider
+rm -rf magento-2-product-slider/.git
 
 rm -rf magento-2-seo
 git clone https://github.com/mageplaza/magento-2-seo
+rm -rf magento-2-seo/.git
 
 
 
